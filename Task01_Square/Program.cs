97ff47f1ccc12ab31ca804ef73b0be5c8fe7074e
NKompa <nkompa7@gmail.com>
2022-08-22 00:25:20 +0300

    Task 1 Square

---
+++ Task01_Square/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат. 4 - 16, 3 - 9 и т.д.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine(sqr);
