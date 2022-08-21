8b0161713f8c1400141003f9ec693ac10c4df7e7
NKompa <nkompa7@gmail.com>
2022-08-22 00:34:50 +0300

    Task 4 Number Range

---
+++ Task04_NumRange/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N. 2: -2,-1,0,1,2
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+int numberNeg = number*(-1);
+
+while(numberNeg<=number)
+{
+    Console.WriteLine(numberNeg);
+    numberNeg++;
+}
