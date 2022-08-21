7f1124667c25b5dd6cc6f0839a9f98722f97d58b
NKompa <nkompa7@gmail.com>
2022-08-22 00:37:13 +0300

    Task 5 Last Number

---
+++ Task05_LastNum/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа. 456 -6
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberLast = number%10;
+Console.WriteLine(numberLast);
